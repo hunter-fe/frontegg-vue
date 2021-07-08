3239e3b8f376a775251d7bd477e5390c2e5ae384
Hunter Weinsheink <hunter@frontegg.com>
2021-07-08 11:45:36 +0300

    updating

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <div id="app">
+  <div id="app" v-if="fronteggLoaded">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>
+      Logged in as
+      {{ this.authState.user ? this.authState.user.email : "Not logged in" }}
+    </p>
+    <button v-on:click="showAdminPortal">Open admin portal</button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { openAdminPortal } from "@frontegg/vue";
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      ...this.mapAuthState(),
+    };
+  },
+  methods: {
+    showAdminPortal() {
+      openAdminPortal();
+    },
+  },
 }
 </script>
 
